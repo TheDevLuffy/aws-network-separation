f5451f371945ee07d4e85129e3bb37fba715666c
thedevluffy <erased11@gmail.com>
2019-10-07 23:18:56 +0900

    test: delivery 모델 테스트 추가

---
+++ test/models/delivery.spec.js
@@ -0,0 +1,25 @@
+const {
+  sequelize,
+  dataTypes,
+  checkModelName,
+  checkPropertyExists
+} = require('sequelize-test-helpers')
+
+const Delivery = require('../../src/models/delivery')
+
+describe('Delivery.js', () => {
+  const DeliveryModel = Delivery(sequelize, dataTypes)
+  const delivery = new DeliveryModel()
+
+  it('테이블의 이름이 Delivery 이다.', () => {
+    checkModelName(DeliveryModel)('Delivery')
+  })
+
+  context('Delivery 모델이 정상적으로 생성된다.', () => {
+    [
+      'address',
+      'orderer',
+      'menu'
+    ].forEach(checkPropertyExists(delivery))
+  })
+})
